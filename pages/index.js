4e0024d1befa9b8637cfcf259ed6014730aca089
AlexSna <asnatschko@gmail.com>
2022-09-22 22:57:31 +0200

    added ul with one story as li

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,19 @@ import styles from '../styles/Home.module.css';
 import styled from 'styled-components';
 
 export default function Home() {
-  return <h1>Readme - App</h1>;
+  return (
+    <>
+      <h1>Readme - App</h1>
+      <ul>
+        <li>
+          Olaf ist das faulste Pferd das es gibt. Er macht krank, doch als Anna
+          das Reitkind vor ihm steht und weint, bekommt er ein schlechtes
+          Gewissen. Hier in meiner lustigen Kindergeschichte erfahrt ihr, wie es
+          ausgeht.
+        </li>
+      </ul>
+    </>
+  );
 }
 
 const Button = styled.button`
