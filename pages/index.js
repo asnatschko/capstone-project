d801fa7aca3e170564294ef96e906aa451de3b08
AlexSna <asnatschko@gmail.com>
2022-09-22 22:43:38 +0200

    added h1-tag with ReadMe

--- pages/index.js
+++ pages/index.js
@@ -2,9 +2,10 @@ import Head from 'next/head';
 import Image from 'next/image';
 import styles from '../styles/Home.module.css';
 import styled from 'styled-components';
+import Card from '../components/Card';
 
 export default function Home() {
-  return <h1>Readme - App</h1>;
+  return <h1>ReadMe</h1>;
 }
 
 const Button = styled.button`
