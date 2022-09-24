25152f98e790223dac762b5e5a86f7974bce2edf
AlexSna <asnatschko@gmail.com>
2022-09-24 13:50:44 +0200

    minor change for vercel

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import Link from 'next/link';
 export default function Home() {
   return (
     <>
-      <h1>Readme - App</h1>
+      <h1>Readme-App</h1>
 
       <Card />
       <Link href="/nextstory">Next Story</Link>
