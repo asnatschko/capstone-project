afd28e3f0789f5836a4d8e9efde0a8beff52e446
AlexSna <asnatschko@gmail.com>
2022-09-29 14:48:11 +0200

    bugfix single story page

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,15 @@
 import Card from '../components/Card';
 import Link from 'next/link';
 import styled from 'styled-components';
+import { shortStories } from './stories';
 
 export default function Home() {
+  const firstStory = shortStories;
+
   return (
     <>
       <StoryContainer>
-        <Card content="Once upon a time there was a girl. She liked playing with her sister. And one day, when their parents were gone, they found a secret room in the house." />
+        <Card content={firstStory[0].content} id={firstStory[0].id} />
         <div>
           <Link className="nextStoryButton" href="/nextstory">
             Next Story
