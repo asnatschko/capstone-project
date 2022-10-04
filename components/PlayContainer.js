834f64ebdae45ceb0cad9e92460c3a806b503ed0
AlexSna <asnatschko@gmail.com>
2022-10-04 15:22:01 +0200

    add playbutton logic

--- components/PlayContainer.js
+++ components/PlayContainer.js
@@ -1,17 +1,8 @@
 import Typewriter from 'typewriter-effect';
-import { id } from '../pages/story/[id].js';
 import { shortStories } from '../pages/stories.js';
 
-export default function PlayContainer() {
-  // let playstory = shortStories.find((story) => story.id === id);
-
-  function getId(shortStories, id) {
-    let obj = shortStories.filter(function (val) {
-      return val.id === id;
-    });
-    return obj[0];
-  }
-
+export default function PlayContainer({ storyId }) {
+  let playedstory = shortStories.find((story) => story.id === storyId);
   return (
     <>
       <h2>Read and Listen</h2>
@@ -20,14 +11,8 @@ export default function PlayContainer() {
           onInit={(typewriter) => {
             typewriter
               .pauseFor(1000)
-              .typeString('Once upon a time there was a girl.')
+              .typeString(playedstory.content)
               .pauseFor(700)
-              .typeString(' She liked playing with her sister.')
-              .pauseFor(700)
-              .typeString(
-                'And one day, when their parents were gone, they found a secret room in the house.'
-              )
-              .pauseFor(1000)
               .start();
           }}
         />
