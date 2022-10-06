0779866131dde6bcdba84f79d0ddf467c718b849
AlexSna <asnatschko@gmail.com>
2022-10-06 17:19:22 +0200

    try to fix the bug

--- pages/playedstory/[id].js
+++ pages/playedstory/[id].js
@@ -3,7 +3,6 @@ import PlayContainer from '../../components/PlayContainer';
 import { useRouter } from 'next/router';
 import ReadAloud from '../../components/ReadAloud';
 
-
 export default function Playedstory() {
   const router = useRouter();
   const id = Number(router.query.id);
@@ -13,7 +12,7 @@ export default function Playedstory() {
   return (
     <StoryContainer>
       <PlayContainer storyId={id} />
-      <ReadAloud storyId={id}/>
+      <ReadAloud storyId={id} />
     </StoryContainer>
   );
 }
