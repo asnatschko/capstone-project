b3b94fa743c2947c32b2fd03dfddbec0e611e443
AlexSna <asnatschko@gmail.com>
2022-10-06 16:09:15 +0200

    delete another typewriter package

--- components/PlayContainer.js
+++ components/PlayContainer.js
@@ -1,4 +1,3 @@
-//import Typewriter from 'typewriter-effect';
 import TypeWriterEffect from 'react-typewriter-effect';
 import { shortStories } from '../pages/stories.js';
 
@@ -15,15 +14,6 @@ export default function PlayContainer({ storyId }) {
           text={playedstory.content}
           typeSpeed={80}
         />
-        {/* "I have to choose between Typewriter effects))" { <Typewriter
-          onInit={(typewriter) => {
-            typewriter
-              .pauseFor(1000)
-              .typeString(playedstory.content)
-              .pauseFor(700)
-              .start();
-          }}
-        /> */}
       </div>
     </>
   );
